e9b4d1d6880f6afd4d068fc6405720c1d6f3f3a3
Frollerman <frollerman@yandex.ru>
2021-09-19 12:53:44 +0300

    Done 3 tasks

---
+++ Ch8/8/8_2/8_2.cpp
@@ -0,0 +1,69 @@
+﻿// strplus.cpp
+// перегрузка операции + для строк
+#include <iostream>
+using namespace std;
+#include <string.h>   // для функций strcpy, strcat
+#include <stdlib.h>   // для функции exit
+///////////////////////////////////////////////////////////
+class String          // наш класс для строк
+{
+private:
+    enum { SZ = 80 }; // максимальный размер строки
+    char str[SZ];  // массив для строки
+public:
+    // конструктор без параметров
+    String()
+    {
+        strcpy_s(str, "");
+    }
+    // конструктор с одним параметром
+    String(const char s[])
+    {
+        strcpy_s(str, s);
+    }
+    // показ строки
+    void display() const
+    {
+        cout << str;
+    }
+    // оператор сложения
+    String operator+= (String ss)
+    {
+        //String temp;  // временная переменная
+        if (strlen(str) + strlen(ss.str) < SZ)
+        {
+            //strcpy_s(temp.str, str);    // копируем содержимое первой строки
+            //strcat_s(temp.str, ss.str); // добавляем содержимое второй строки
+            strcat_s(str, ss.str); // первая строка + вторая строка
+        }
+        else
+        {
+            cout << "\nOverflow!";
+            exit(1);
+        }
+        return str;  // возвращаем результат
+    }
+};
+///////////////////////////////////////////////////////////
+int main()
+{
+    String s1 = "\nMerry Christmas! "; // используем конструктор с параметром
+    String s2 = "Happy New Year!";   // используем конструктор с параметром
+    String s3;                      // используем конструктор без параметров
+
+    // показываем строки
+    s1.display();
+    s2.display();
+    s3.display();
+
+    s3 = s1 += s2;       // присваиваем строке s3 результат сложения строк s1 и s2
+
+    cout << endl << "s3 = "; s3.display();     // показываем результат
+
+    cout << endl << "s1 = "; s1.display();
+    cout << endl;
+
+    return 0;
+}
+
+
---
+++ Ch8/8/8_3/8_3.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class mytime
+{
+private:
+    int h;
+    int m;
+    int s;
+public:
+    mytime() : h(0), m(0), s(0)
+    {}
+    mytime(int hours, int minutes, int seconds) : h(hours), m(minutes), s(seconds)
+    {}
+    void show()const
+    {
+        if (h < 10) cout << "0";
+        cout << h << ":";
+        if (m < 10) cout << "0";
+        cout << m << ":";
+        if (s < 10) cout << "0";
+        cout << s << endl;
+    }
+    /*void add(mytime t1, mytime t2)
+    {
+        h = t1.h + t2.h;
+        m = t1.m + t2.m;
+        s = t1.s + t2.s;
+        if (s > 59)
+        {
+            m++;
+            s -= 60;
+        }
+        if (m > 59)
+        {
+            h++;
+            m -= 60;
+        }
+        while (h > 12) h -= 12;
+    }*/
+    mytime operator+ (mytime t2) const;
+};
+
+mytime mytime::operator+ (mytime t2) const
+{
+    int h1 = h + t2.h;
+    int m1 = m + t2.m;
+    int s1 = s + t2.s;
+    if (s1 > 59)
+    {
+        m1++;
+        s1 -= 60;
+    }
+    if (m1 > 59)
+    {
+        h1++;
+        m1 -= 60;
+    }
+    while (h1 > 12) h1 -= 12;
+    mytime temp(h1, m1, s1);
+    return temp;
+}
+
+int main()
+{
+    const mytime time1(12, 40, 56), time2(5, 25, 34);
+    mytime time3 = time1 + time2;
+    time3.show();
+    //time3.add(time1, time2);
+    //time3.show();
+
+    return 0;
+}
